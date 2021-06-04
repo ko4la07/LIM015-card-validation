ebaf1f2ab57732697ff68a70f7b974528a5c1d56
Dayana Huaytalla <dhuaytalla@imca.edu.pe>
2021-06-04 03:10:19 -0500

    Ocultar y mostrar partes del HTML

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-import validator from './validator.js';
+// import validator from './validator.js';
+  
+// console.log(validator);
 
-console.log(validator);
 
 
+function cambiadisplay(id){
+    if (id == 'dos'){
+    document.getElementById('uno').style.display='none'; 
+    document.getElementById('dos').style.display='block'; 
+    document.getElementById('tres').style.display='none';
+    }else if (id == 'tres'){
+    document.getElementById('uno').style.display='none';
+    document.getElementById('dos').style.display='none';  
+    document.getElementById('tres').style.display='block';
+    }
+    }
+
+              
+           
