88830f956baeb19427f0160a8647c24c8fe8cbb9
Dayana Huaytalla <dhuaytalla@imca.edu.pe>
2021-06-05 00:22:32 -0500

    nValidación de tarjeta

--- src/validator.js
+++ src/validator.js
@@ -1,28 +1,30 @@
 // import {revArr} from './index'
-
 const validator = {
-  isValid: function (val) {
-    // let sum = 0;
-    // for (let i = 0; i < val.length; i++) {
-    //   let intVal = parseInt(val.substr(i,1));
-    //   if (i%2 == 0) {
-    //     intVal *= 2;
-    //     if (intVal>9) {
-    //       intVal = 1+ (intVal % 10);
-    //     }
-    //   }
-    //   sum += intVal;
-    // }
-    // return (sum %10) == 0;
-
-   return true;
+  isValid: function (cardNumber) {
+    // cardNumber = document.getElementById("cardNumber").value;
+    let cardNumberStr = cardNumber.toString();
+    let card = Array.from(cardNumberStr, x=>parseInt(x));
+    let tarjeta = card.reverse();
+    let sum = 0;
+    for(let i=0; i < tarjeta.length; i++){
+      let intVal = tarjeta[i];
+      if(i % 2 != 0){
+        intVal *= 2;
+        if(intVal > 9){
+          intVal = 1+ (intVal % 10);
+        }
+      }
+      sum += intVal;
+    }
+    if (sum % 10 === 0 && sum !== 0){
+      return true;
+    } 
+    else{
+      return false;
+    }
+  },
+  maskify: function (cardNumber){
+    return cardNumber.length
   }
 };
-
-
-
-
-  
-
-
 export default validator;
