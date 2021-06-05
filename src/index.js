2b9e3c4de453fead363ec4d71a3395828a07eae8
Dayana Huaytalla <dhuaytalla@imca.edu.pe>
2021-06-04 19:39:20 -0500

    números de la tarjeta en un array

--- src/index.js
+++ src/index.js
@@ -1,20 +1,57 @@
-// import validator from './validator.js';
-  
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* import validator from './validator.js'; */
+
+let cardNumber;
+let arr;
+let revArr;
+
+
+/* window.cambiadisplay = function (id) {
+  cardNumber = document.getElementById("cardNumber").value;
+  arr = Array.from(cardNumber,x=>parseInt(x));
+  console.log(arr);
+  revArr = arr.reverse();
+   console.log(revArr);
+  console.log(cardNumber);
+
+  if (id == "dos" && validator.isValid(revArr) == true) {
+    document.getElementById("uno").style.display = "none";
+    document.getElementById("dos").style.display = "block";
+    document.getElementById("tres").style.display = "none";
+  } else if (id == "tres") {
+    document.getElementById("uno").style.display = "none";
+    document.getElementById("dos").style.display = "none";
+    document.getElementById("tres").style.display = "block";
+  }
+} */
+
+
+function cambiadisplay(id) {
+  cardNumber = document.getElementById("cardNumber").value;
+  arr = Array.from(cardNumber,x=>parseInt(x));
+  console.log(arr);
+  revArr = arr.reverse();
+   console.log(revArr);
+  console.log(cardNumber);
+
+  if (id == "dos") {
+    document.getElementById("uno").style.display = "none";
+    document.getElementById("dos").style.display = "block";
+    document.getElementById("tres").style.display = "none";
+  } else if (id == "tres") {
+    document.getElementById("uno").style.display = "none";
+    document.getElementById("dos").style.display = "none";
+    document.getElementById("tres").style.display = "block";
+  }
+}
+
+
+function onlyNumbers(e) {
+  return e.charCode >= 48 && e.charCode <= 57;
+}
+
+
+
 // console.log(validator);
 
-
-
-function cambiadisplay(id){
-    if (id == 'dos'){
-    document.getElementById('uno').style.display='none'; 
-    document.getElementById('dos').style.display='block'; 
-    document.getElementById('tres').style.display='none';
-    }else if (id == 'tres'){
-    document.getElementById('uno').style.display='none';
-    document.getElementById('dos').style.display='none';  
-    document.getElementById('tres').style.display='block';
-    }
-    }
-
-              
-           
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,28 @@
+// import {revArr} from './index'
+
 const validator = {
-  // ...
+  isValid: function (val) {
+    // let sum = 0;
+    // for (let i = 0; i < val.length; i++) {
+    //   let intVal = parseInt(val.substr(i,1));
+    //   if (i%2 == 0) {
+    //     intVal *= 2;
+    //     if (intVal>9) {
+    //       intVal = 1+ (intVal % 10);
+    //     }
+    //   }
+    //   sum += intVal;
+    // }
+    // return (sum %10) == 0;
+
+   return true;
+  }
 };
+
+
+
+
+  
+
 
 export default validator;
