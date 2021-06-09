45ac082365ee1892f4cfe33f650545b0d897f759
Dayana Huaytalla <dhuaytalla@imca.edu.pe>
2021-06-09 00:25:39 -0500

    Alert no ingreso numeros + botones de retorno

--- src/index.js
+++ src/index.js
@@ -8,6 +8,32 @@ document.getElementById("btnValidacion").addEventListener('click', function(){
   changeDisplay('dos');
 });
 
+//-------
+
+document.getElementById("btnRegresarValidacion").addEventListener('click', function(){
+  changeDisplay('uno');
+});
+
+document.getElementById("btnHome").addEventListener('click', function(){
+  changeDisplay('cero');
+});
+
+//------------------
+
+// let cardNumber = document.getElementById("cardNumber").value;
+
+// document.getElementById("btnValidacion").addEventListener('click',function(){ cardNumber.length == 0 ? (prompt('Debes ingresar tu número de tarjeta') & changeDisplay('uno')) : changeDisplay('dos')});
+
+// document.getElementById("btnValidacion").addEventListener('click',function(){
+//   if (cardNumber.length != 0){
+//     changeDisplay('dos')
+//   } 
+//   alert('Debes ingresar tu número de tarjeta') && changeDisplay('uno')
+// });
+
+
+//-----------------
+
 document.getElementById("btnFinalizar").addEventListener('click', function(){
   changeDisplay('tres');
 });
@@ -28,24 +54,52 @@ function changeDisplay(id) {
 
 let cardNumber = document.getElementById("cardNumber").value;
 // console.log(cardNumber);
+
 let res = validator.isValid(cardNumber);
 // console.log(res);
 
 let masked = validator.maskify(cardNumber);
 // console.log(masked);
 
-if (id == "uno") {
+if (id == "cero") {
+  document.getElementById("cero").style.display = "block";
+  document.getElementById("uno").style.display = "none";
+  document.getElementById("dos").style.display = "none";
+  document.getElementById("tres").style.display = "none";
+} else if (id == "uno") {
   document.getElementById("cero").style.display = "none";
   document.getElementById("uno").style.display = "block";
   document.getElementById("dos").style.display = "none";
   document.getElementById("tres").style.display = "none";
 } else if (id == "dos") {
-  document.getElementById("cero").style.display = "none";
+  if(cardNumber.length != 0){
+    document.getElementById("cero").style.display = "none";
     document.getElementById("uno").style.display = "none";
     document.getElementById("dos").style.display = "block";
     document.getElementById("displayResultado").innerHTML = res ? "Tu tarjeta es válida!!!" : "Tu tarjeta no es válida";
     document.getElementById("masked").innerHTML = masked;
     document.getElementById("tres").style.display = "none";
+    
+    if (res == true) {
+      document.getElementById("dos-btnFinalizar").style.display="block";
+      document.getElementById("dos-btnRegresarValidacion").style.display="none";
+    } else{
+      document.getElementById("dos-btnRegresarValidacion").style.display="block";
+      document.getElementById("dos-btnFinalizar").style.display="none";
+    }
+
+  } else{
+    alert('Debes ingresar tu número de tarjeta');
+    return (id == "uno");
+  }
+
+  //----------------------------
+  // document.getElementById("cero").style.display = "none";
+  //   document.getElementById("uno").style.display = "none";
+  //   document.getElementById("dos").style.display = "block";
+  //   document.getElementById("displayResultado").innerHTML = res ? "Tu tarjeta es válida!!!" : "Tu tarjeta no es válida";
+  //   document.getElementById("masked").innerHTML = masked;
+  //   document.getElementById("tres").style.display = "none";
   } else if (id == "tres") {
     document.getElementById("cero").style.display = "none";
     document.getElementById("uno").style.display = "none";
