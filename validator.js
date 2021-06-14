60423f71ef09ebc3bce9773bb3eebf4d55dd02a6
Dayana Huaytalla <dhuaytalla@imca.edu.pe>
2021-06-14 11:21:07 -0500

    Updates

--- validator.js
+++ validator.js
@@ -1,9 +1,7 @@
 
 const validator = {
   isValid: function (cardNumber) {
-    // cardNumber = document.getElementById("cardNumber").value;
-    let cardNumberStr = cardNumber.toString();
-    let cardArr = Array.from(cardNumberStr, x=>parseInt(x));
+    let cardArr = Array.from(cardNumber, x=>parseInt(x));
     let cardArrRev = cardArr.reverse();
     let sum = 0;
     for(let i=0; i < cardArrRev.length; i++){
@@ -24,28 +22,24 @@ const validator = {
     }
   },
   maskify: function (cardNumber){
-    let cardNumberStr = cardNumber.toString();
     let maskedNumbers = '';
-    let len = cardNumberStr.length;
+    let len = cardNumber.length;
     if(len > 4){
-      let visibleNumbers = cardNumberStr.substring(len - 4, len);
+      let visibleNumbers = cardNumber.substring(len - 4, len);
       for(let i=0; i < len - 4; i++){
         maskedNumbers = maskedNumbers + "#";
       }
       return maskedNumbers + visibleNumbers;
     } else{
-      return cardNumberStr;
+      return cardNumber;
     }
   },
   getIssuer: function(cardNumber){
     if (parseInt(cardNumber.substring(0,1)) === 4 && 13<= cardNumber.length && cardNumber.length <=16) {
-      // document.getElementById('franquicia-1').innerHTML= 'visa';
       return 'VISA';
     } else if ((parseInt(cardNumber.substring(0,2))=== 34 || parseInt(cardNumber.substring(0,2))=== 37) && cardNumber.length==15){
-      // document.getElementById('franquicia-2').innerHTML= 'American Express';
       return 'American Express'
     } else if (50 < parseInt(cardNumber.substring(0,2)) && parseInt(cardNumber.substring(0,2)) < 56 && cardNumber.length==16){
-      // document.getElementById('franquicia-3').innerHTML= 'MasterCard';
       return 'MasterCard';
     }
   } 
